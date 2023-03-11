4470e16a3a7652aa8ccb49f2e7f7127ae3023cc2
Deborah <110403802+deborahsom@users.noreply.github.com>
2023-03-11 17:46:40 +0700

    Create Main.java

---
+++ main/Main.java
@@ -0,0 +1,31 @@
+package main;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+        JFrame frame;
+        GamePanel gp;
+
+        // cr eate frame to display
+        frame = new JFrame("Harry Potter Far From Hogwarts");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // initialize main.GamePanel
+        gp = new GamePanel();
+
+        // add icon to game
+        frame.setIconImage(new ImageIcon("src/img/harry.png").getImage());
+
+
+        // add the component to the frame
+        frame.add(gp);
+        frame.pack();
+        frame.setResizable(false);
+        frame.setSize(gp.PANEL_WIDTH, gp.PANEL_HEIGHT);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+
+}
